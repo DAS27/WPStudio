455d179edf99b4535c519f812fcfa111623a7a85
Damir <sarkulindamir@gmail.com>
2020-10-28 14:26:12 +0500

    fixed code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     public function images()
     {
-        return $this->hasMany('App\ProductImage');
+        return $this->hasMany('App\Models\ProductImage');
     }
 }
