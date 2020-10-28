91945947406eb42f89fa5580f0f6d37412309fe1
Damir <sarkulindamir@gmail.com>
2020-10-28 14:29:17 +0500

    added removeFromCart method

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,6 +9,11 @@ class CartController extends Controller
 {
     public function index()
     {
+        if (!is_null($_COOKIE['cart_id'])) {
+            $cartId = $_COOKIE['cart_id'];
+            $item = \Cart::session($cartId)->getContent()->first();
+            return view('cart.cart', compact('item'));
+        }
         return view('cart.cart');
     }
 
@@ -16,19 +21,31 @@ class CartController extends Controller
     {
         $product = Product::where('id', $request->id)->first();
         if (!isset($_COOKIE['cart_id'])) {
-            setcookie('cart_id', uniqid());
+            $cartId = setcookie('cart_id', uniqid());
+        } else {
+            $cartId = $_COOKIE['cart_id'];
         }
-        $cartId = $_COOKIE['cart_id'];
         \Cart::session($cartId);
 
         \Cart::add([
             'id' => $product->id,
             'name' => $product->title,
-            'price' => $product->price_new ? $product->price_new : $product->price,
+            'price' => (int) $product->price_new ? $product->price_new : $product->price,
             'quantity' => (int) $request->qty,
-            'attributes' => [],
+            'attributes' => [
+                'img_s' => $product->images[0]->img_small,
+                'img_l' => $product->images[0]->img_large
+            ],
         ]);
 
         return response()->json(\Cart::getContent());
+    }
+
+    public function removeFromCart()
+    {
+        $cartId = $_COOKIE['cart_id'];
+        $product = \Cart::session($cartId)->getContent()->first();
+        \Cart::session($cartId)->remove($product->id);
+        return view('cart.cart');
     }
 }
