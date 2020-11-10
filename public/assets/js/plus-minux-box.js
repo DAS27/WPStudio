3b1d3a83e090e33d984b3ea293b6a08d5fe663ee
Damir <sarkulindamir@gmail.com>
2020-11-10 14:43:14 +0500

    fixed code

--- public/assets/js/plus-minux-box.js
+++ public/assets/js/plus-minux-box.js
@@ -2,18 +2,18 @@ window.initPlusMinusBox = function () {
     /*----------------------------
     	Cart Plus Minus Button
     ------------------------------ */
-    var CartPlusMinus = $('.cart-plus-minus');
+    let CartPlusMinus = $('.cart-plus-minus');
     CartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
     CartPlusMinus.append('<div class="inc qtybutton">+</div>');
     $(".qtybutton").on("click", function () {
-        var newVal;
-        var $button = $(this);
-        var oldValue = $button.parent().find("input").val();
+        let newVal;
+        let $button = $(this);
+        let oldValue = $button.parent().find("input").val();
         if ($button.text() === "+") {
             newVal = parseFloat(oldValue) + 1;
         } else {
             // Don't allow decrementing below zero
-            if (oldValue > 0) {
+            if (oldValue > 1) {
                 newVal = parseFloat(oldValue) - 1;
             } else {
                 newVal = 1;
